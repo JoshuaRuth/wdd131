0c7b7894fdd6e86236ef8923454212228c8a14af
JoshuaRuth <Joshuaruth1999@gmail.com>
2025-09-13 02:20:02 -0600

    commiting for basic page audit

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+//footer code start
+// select the DOM elements for output
+const year = document.querySelector("#year");
+
+// use the date object
+const today = new Date();
+
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}© ♋Joshua Ruth ☢ Utah, United States of America</span>`;
+
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
+//footer code end
