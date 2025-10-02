afc057e6501e2782d876f1a7a77c56ca2d9f60dc
JoshuaRuth <Joshuaruth1999@gmail.com>
2025-10-02 16:41:51 -0600

    Basic website stuff for fina project.

---
+++ project/scripts/project.js
@@ -0,0 +1,12 @@
+// select the DOM elements for output
+const year = document.querySelector("#year");
+
+// use the date object
+const today = new Date();
+
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}© ♋Joshua Ruth ☢ Utah, United States of America</span>`;
+
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
+//==============================================================================//
