4e00f55d4cdc385f34900c41c0a9e03a30eb62bb
JoshuaRuth <Joshuaruth1999@gmail.com>
2025-10-06 22:03:58 -0600

    Changed review value

--- scripts/form.js
+++ scripts/form.js
@@ -10,3 +10,39 @@ year.innerHTML = `<span class="highlight">${today.getFullYear()}© ♋Joshua Rut
 let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
 //
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+const select = document.querySelector("#selectProduct");
+
+  
+products.forEach(product =>{
+const option = document.createElement("option");
+option.value = product.id;
+option.textContent = product.name;
+select.appendChild(option)
+});
