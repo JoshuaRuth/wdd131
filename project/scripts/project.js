86050efcbe6ea279964913e08d9fc50c4957c0e7
JoshuaRuth <Joshuaruth1999@gmail.com>
2025-10-08 06:19:39 -0600

    added form to index and code for form to project.js

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -12,5 +12,50 @@ document.getElementById("lastModified").innerHTML = text;
 //==============================================================================//
 // Hamburger Menu?
 
+//select for form
+const gems = [
+  {
+    name: "Ruby",
+  },
+  {
+    name: "Topaz",
+  },
+  {
+    name: "Citrine",
+  },
+  {
+    name: "Emerald",
+  },
+  {
+    name: "Sapphire",
+  },
+  {
+    name: "Tanzanite",
+  },
+  {
+    name: "Amethyst",
+  },
+  {
+    name: "Opal",
+  },
+    {
+    name: "Smokey Quartz",
+  },
+  {
+    name: "Other (Please let us know in the text box...)",
+  },
+];
+
+const select = document.querySelector("#selectGem");
+  
+gems.forEach(gem =>{
+  let option = document.createElement("option");
+
+  option.value = gem.name;
+  
+  option.textContent = gem.name;
+
+  select.appendChild(option)
+});
 //==Rock gallery==//
 
